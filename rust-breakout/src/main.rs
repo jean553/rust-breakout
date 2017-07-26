2d696a60d326b10cfb8d0071de2372a0bd646091
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-26 13:39:23 +0200

    display the piston window

--- rust-breakout/src/main.rs
+++ rust-breakout/src/main.rs
@@ -1,3 +1,19 @@
+extern crate piston_window;
+
+use piston_window::{
+    PistonWindow,
+    WindowSettings,
+};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut window: PistonWindow = WindowSettings::new(
+        "Rust Breakout",
+        [600, 400]
+    )
+    .build()
+    .unwrap();
+
+    while let Some(event) = window.next() {
+    }
 }
