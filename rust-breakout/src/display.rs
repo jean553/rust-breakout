9bbe9b9686b6d7048213b1c757a47c6f71573bde
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-28 21:23:38 +0200

    delete useless code and documentation

--- rust-breakout/src/display.rs
+++ rust-breakout/src/display.rs
@@ -2,9 +2,6 @@
 //!
 //! This trait is implemented by every game-level defined
 //! (and not library-level) displayable components.
-//!
-//! TODO: this trait is only used by one item for now;
-//! it should be implemented for the player surface as well.
 
 use piston_window::G2d;
 
