20133dfa6f2c1a9711c3dfd26f02bb277eb0495c
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-28 13:45:22 +0200

    integrate the separators module

---
+++ rust-breakout/src/separators.rs
@@ -0,0 +1,71 @@
+use piston_window::G2d;
+
+use graphics::rectangle::Rectangle;
+use graphics::context::Context;
+
+use display::Display;
+
+pub struct Separators {
+    left: Rectangle,
+    right: Rectangle,
+}
+
+impl Separators {
+
+    pub fn new() -> Separators {
+
+        const SEPARATOR_WHITE_COLOR: [f32; 4] = [
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+        ];
+
+        Separators {
+            left: Rectangle::new(SEPARATOR_WHITE_COLOR),
+            right: Rectangle::new(SEPARATOR_WHITE_COLOR),
+        }
+    }
+}
+
+impl Display for Separators {
+
+    fn display(
+        &self,
+        context: Context,
+        graphics: &mut G2d,
+    ) {
+
+        const SEPARATOR_VERTICAL_POSITION: f64 = 0.0;
+        const SEPARATOR_WIDTH: f64 = 1.0;
+        const SEPARATOR_HEIGHT: f64 = 1600.0;
+
+        const LEFT_SEPARATOR_HORIZONTAL_POSITION: f64 = 300.0;
+
+        self.left.draw(
+            [
+                LEFT_SEPARATOR_HORIZONTAL_POSITION,
+                SEPARATOR_VERTICAL_POSITION,
+                SEPARATOR_WIDTH,
+                SEPARATOR_HEIGHT,
+            ],
+            &context.draw_state,
+            context.transform,
+            graphics,
+        );
+
+        const RIGHT_SEPARATOR_HORIZONTAL_POSITION: f64 = 1300.0;
+
+        self.right.draw(
+            [
+                RIGHT_SEPARATOR_HORIZONTAL_POSITION,
+                SEPARATOR_VERTICAL_POSITION,
+                SEPARATOR_WIDTH,
+                SEPARATOR_HEIGHT,
+            ],
+            &context.draw_state,
+            context.transform,
+            graphics,
+        );
+    }
+}
