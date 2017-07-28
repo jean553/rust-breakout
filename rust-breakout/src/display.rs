6d3736718465af783b9f09109115d5247952ff56
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-28 14:02:51 +0200

    integrate documentation into the display module

--- rust-breakout/src/display.rs
+++ rust-breakout/src/display.rs
@@ -1,9 +1,18 @@
+//! Declaration of trait `Display`
+//!
+//! This trait is implemented by every game-level defined
+//! (and not library-level) displayable components.
+//!
+//! TODO: this trait is only used by one item for now;
+//! it should be implemented for the player surface as well.
+
 use piston_window::G2d;
 
 use graphics::context::Context;
 
 pub trait Display {
 
+    /// Displays the component.
     fn display(
         &self,
         Context,
